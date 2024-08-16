fece8cf3b5af80797420a974a0563f503f72d88e
Michał Zadrożny <mt.zadrozny@gmail.com>
2024-08-16 14:44:14 +0200

    Add insert operation with tests

---
+++ src/main/java/pl/mzadrozny/RedBlackTree.java
@@ -0,0 +1,193 @@
+package pl.mzadrozny;
+
+
+import static pl.mzadrozny.Node.BLACK;
+import static pl.mzadrozny.Node.RED;
+
+public class RedBlackTree<T extends Comparable<T>> implements BinarySearchTree<T> {
+
+    private Node<T> root;
+
+    private final boolean allowDuplicates;
+
+    public RedBlackTree() {
+        this.root = NilNode.getInstance();
+        this.allowDuplicates = true;
+    }
+
+    public RedBlackTree(boolean allowDuplicates) {
+        this.root = NilNode.getInstance();
+        this.allowDuplicates = allowDuplicates;
+    }
+
+    @Override
+    public Node<T> getRoot() {
+        return root;
+    }
+
+    @Override
+    public Node<T> searchNode(T key) {
+        return null;
+    }
+
+    //    O(log n)
+    @Override
+    public void insertNode(T data) {
+
+        Node<T> newNode = new Node<>(data);
+        Node<T> parent = null;
+        Node<T> current = root;
+
+        while (current != NilNode.getInstance()) {
+            parent = current;
+
+            if (newNode.getData().compareTo(current.getData()) < 0) {
+                current = current.getLeft();
+            } else if (newNode.getData().compareTo(current.getData()) > 0) {
+                current = current.getRight();
+            } else {
+                if (!allowDuplicates) {
+                    throw new IllegalArgumentException("Tree already contains a node with value: " + data);
+                }
+
+                current = current.getLeft();
+            }
+        }
+
+        newNode.setParent(parent);
+
+        if (parent == null) {
+            root = newNode;
+        } else if (newNode.getData().compareTo(parent.getData()) < 0) {
+            parent.setLeft(newNode);
+        } else {
+            parent.setRight(newNode);
+        }
+
+        insertFixup(newNode);
+    }
+
+    private void insertFixup(Node<T> node) {
+
+        while (node.getParent() != null && node.getParent().getColor() == RED) {
+
+            if (node.getParent() == node.getParent().getParent().getLeft()) {
+                Node<T> current = node.getParent().getParent().getRight();
+
+                if (current.getColor() == RED) {
+                    node.getParent().setColor(BLACK);
+                    current.setColor(BLACK);
+                    node.getParent().getParent().setColor(RED);
+                    node = node.getParent().getParent();
+                } else {
+                    if (node == node.getParent().getRight()) {
+                        node = node.getParent();
+                        rotateLeft(node);
+                    }
+
+                    node.getParent().setColor(BLACK);
+                    node.getParent().getParent().setColor(RED);
+                    rotateRight(node.getParent().getParent());
+                }
+            } else {
+                Node<T> current = node.getParent().getParent().getLeft();
+
+                if (current.getColor() == RED) {
+                    node.getParent().setColor(BLACK);
+                    current.setColor(BLACK);
+                    node.getParent().getParent().setColor(RED);
+                    node = node.getParent().getParent();
+                } else {
+                    if (node == node.getParent().getLeft()) {
+                        node = node.getParent();
+                        rotateRight(node);
+                    }
+
+                    node.getParent().setColor(BLACK);
+                    node.getParent().getParent().setColor(RED);
+                    rotateLeft(node.getParent().getParent());
+                }
+            }
+
+            if (node == root) {
+                break;
+            }
+        }
+
+        root.setColor(BLACK);
+    }
+
+
+    @Override
+    public void deleteNode(T key) {
+
+    }
+
+    private void rotateRight(Node<T> node) {
+        Node<T> parent = node.getParent();
+        Node<T> leftChild = node.getLeft();
+
+        node.setLeft(leftChild.getRight());
+        if (leftChild.getRight() != null) {
+            leftChild.getRight().setParent(node);
+        }
+
+        leftChild.setRight(node);
+        node.setParent(leftChild);
+
+        replaceParentsChild(parent, node, leftChild);
+    }
+
+    private void rotateLeft(Node<T> node) {
+        Node<T> parent = node.getParent();
+        Node<T> rightChild = node.getRight();
+
+        node.setRight(rightChild.getLeft());
+        if (rightChild.getLeft() != null) {
+            rightChild.getLeft().setParent(node);
+        }
+
+        rightChild.setLeft(node);
+        node.setParent(rightChild);
+
+        replaceParentsChild(parent, node, rightChild);
+    }
+
+    private void replaceParentsChild(Node<T> parent, Node<T> oldChild, Node<T> newChild) {
+        if (parent == null) {
+            root = newChild;
+        } else if (parent.getLeft() == oldChild) {
+            parent.setLeft(newChild);
+        } else if (parent.getRight() == oldChild) {
+            parent.setRight(newChild);
+        } else {
+            throw new IllegalStateException("Node is not a child of its parent");
+        }
+
+        if (newChild != null) {
+            newChild.setParent(parent);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        appendNodeToStringRecursive(getRoot(), builder);
+        return builder.toString();
+    }
+
+    private void appendNodeToStringRecursive(Node<T> node, StringBuilder builder) {
+        builder.append(node.getData());
+
+        if (node.getLeft() != null) {
+            builder.append(" L{");
+            appendNodeToStringRecursive(node.getLeft(), builder);
+            builder.append('}');
+        }
+        if (node.getRight() != null) {
+            builder.append(" R{");
+            appendNodeToStringRecursive(node.getRight(), builder);
+            builder.append('}');
+        }
+    }
+}
---
+++ src/test/java/pl/mzadrozny/RedBlackTreeTest.java
@@ -0,0 +1,146 @@
+package pl.mzadrozny;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RedBlackTreeTest {
+
+    @Test
+    void should_addAsRoot_when_addingFirstElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+
+//        then
+        assertEquals(tree.getRoot().getData(), Integer.valueOf(5));
+    }
+
+    @Test
+    void should_addRootAsBlack_when_addingFirstElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+
+//        then
+        assertFalse(tree.getRoot().getColor());
+    }
+
+    @Test
+    void should_addToTheLeft_when_addingSmallerElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+        tree.insertNode(1);
+
+//        then
+        assertEquals(tree.getRoot().getLeft().getData(), Integer.valueOf(1));
+    }
+
+    @Test
+    void should_addToTheRight_when_addingBiggerElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+        tree.insertNode(6);
+
+//        then
+        assertEquals(tree.getRoot().getRight().getData(), Integer.valueOf(6));
+    }
+
+    @Test
+    void should_fixTree_when_addingSecondSmallerElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+        tree.insertNode(3);
+        tree.insertNode(1);
+
+//        then
+        assertEquals(tree.getRoot().getData(), Integer.valueOf(3));
+        assertEquals(tree.getRoot().getLeft().getData(), Integer.valueOf(1));
+        assertEquals(tree.getRoot().getRight().getData(), Integer.valueOf(5));
+    }
+
+    @Test
+    void should_fixTree_when_addingSecondBiggerElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+        tree.insertNode(7);
+        tree.insertNode(9);
+
+//        then
+        assertEquals(tree.getRoot().getData(), Integer.valueOf(7));
+        assertEquals(tree.getRoot().getLeft().getData(), Integer.valueOf(5));
+        assertEquals(tree.getRoot().getRight().getData(), Integer.valueOf(9));
+    }
+
+    @Test
+    void should_fixTree_when_addingBiggerAndThenSmallerElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+        tree.insertNode(10);
+        tree.insertNode(9);
+
+//        then
+        assertEquals(tree.getRoot().getData(), Integer.valueOf(9));
+        assertEquals(tree.getRoot().getLeft().getData(), Integer.valueOf(5));
+        assertEquals(tree.getRoot().getRight().getData(), Integer.valueOf(10));
+    }
+
+    @Test
+    void should_fixTree_when_addingSmallerAndThenBiggerElement() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+        tree.insertNode(1);
+        tree.insertNode(10);
+
+//        then
+        assertEquals(tree.getRoot().getData(), Integer.valueOf(5));
+        assertEquals(tree.getRoot().getLeft().getData(), Integer.valueOf(1));
+        assertEquals(tree.getRoot().getRight().getData(), Integer.valueOf(10));
+    }
+
+    @Test
+    void should_throw_when_addingDuplicate_given_treeNotAllowDuplicates() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>(false);
+
+//        when
+        tree.insertNode(5);
+
+//        then
+        assertThrows(IllegalArgumentException.class, () -> tree.insertNode(5));
+    }
+
+    @Test
+    void should_notThrow_when_addingDuplicate_given_treeNotAllowDuplicates() {
+//        given
+        RedBlackTree<Integer> tree = new RedBlackTree<>();
+
+//        when
+        tree.insertNode(5);
+
+//        then
+        assertDoesNotThrow(() -> tree.insertNode(5));
+    }
+}
